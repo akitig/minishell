544a4487b4be5173f24d7674c1d0b193c07e8bd6
akitig <akitig24@gmail.com>
2025-01-26 16:37:04 +0900

    add libft,gnl,printf

---
+++ minishell/libft/gnl/get_next_line_utils.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akunimot <akitig24@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 14:37:50 by akunimot          #+#    #+#             */
+/*   Updated: 2025/01/26 16:32:49 by akunimot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+// ft_memset(s, 0, n);
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*ans;
+
+	i = 0;
+	ans = (unsigned char *)s;
+	while (i < n)
+	{
+		ans[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*ptr;
+
+	if (size > 0 && (SIZE_MAX / size < count))
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
+	size *= count;
+	ptr = malloc(size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, size);
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (1)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		if (s[i] == '\0')
+			break ;
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_gnl_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[j++] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		str[j++] = s2[i];
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
