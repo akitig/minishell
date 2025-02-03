bc55282f3aa456349f26c52aa3566d784c1d92bf
akitig <akitig24@gmail.com>
2025-02-03 23:15:19 +0900

    Basic Parser

--- minishell/src/parse/split.c
+++ minishell/src/parse/split.c
@@ -6,13 +6,14 @@
 /*   By: akunimot <akitig24@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 16:40:32 by akunimot          #+#    #+#             */
-/*   Updated: 2025/02/01 00:04:03 by akunimot         ###   ########.fr       */
+/*   Updated: 2025/02/03 23:12:41 by akunimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-static int	ft_wordlen(char const *str, char c)
+/*
+static int	ft_word_len(char const *str, char c)
 {
 	unsigned int	i;
 	int				len;
@@ -32,25 +33,6 @@ static int	ft_wordlen(char const *str, char c)
 	}
 	return (len);
 }
-
-char	*ft_strndup(const char *src, size_t n)
-{
-	char	*dst;
-	size_t	i;
-
-	dst = (char *)malloc(sizeof(char) * (n + 1));
-	if (!dst)
-		return (NULL);
-	i = 0;
-	while (i < n && src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
 static char	*ft_allocate_word(const char *str, char c)
 {
 	int		len;
@@ -72,6 +54,41 @@ static void	ft_free_array(char **array, int i)
 	while (i > 0)
 		free(array[--i]);
 	free(array);
+}
+*/
+void	ft_mv_in_quote(char const *str, unsigned int *i)
+{
+	char	quote;
+
+	if (str[*i] == '\"' || str[*i] == '\'')
+	{
+		quote = str[*i];
+		(*i)++;
+		while (str[*i])
+		{
+			if (str[*i] == quote)
+				break ;
+			(*i)++;
+		}
+	}
+}
+
+char	*ft_strndup(const char *src, size_t n)
+{
+	char	*dst;
+	size_t	i;
+
+	dst = (char *)malloc(sizeof(char) * (n + 1));
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (i < n && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
 
 static char	*expand_daller(char *word, char **env)
@@ -103,37 +120,186 @@ static void	input_daller(char **str, char **env)
 	}
 }
 
-char	**ft_split_str(char const *str, char c)
+int	ft_char_len(char const *s, char c)
 {
-	char	**array;
-	int		words;
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != c && s[i])
+	{
+		ft_mv_in_quote(s, &i);
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_word_num(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	flg;
+	int				num;
+
+	i = 0;
+	flg = 0;
+	num = 0;
+	while (s[i])
+	{
+		ft_mv_in_quote(s, &i);
+		if (s[i] != c)
+			flg = 1;
+		else if (flg && s[i] == c)
+		{
+			num++;
+			flg = 0;
+		}
+		i++;
+	}
+	if (flg)
+		num++;
+	return (num);
+}
+
+static char	*ft_fill_word(char *word, char *str, int len)
+{
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static int	ft_split_low(char **array, char const *s, char c)
+{
+	char	*str;
+	char	*word;
+	int		len;
 	int		i;
 
-	words = ft_wordlen(str, c);
-	array = malloc(sizeof(char *) * (words + 1));
-	if (!array)
-		return (NULL);
+	str = (char *)s;
 	i = 0;
-	while (*str)
+	while (i < ft_word_num(s, c))
 	{
-		if (*str != c)
+		len = ft_char_len(str, c);
+		if (len > 0)
 		{
-			array[i] = ft_allocate_word(str, c);
-			if (!array[i])
-				return (ft_free_array(array, i), NULL);
-			while (*str && *str != c)
-				str++;
+			word = malloc(sizeof(char) * (len + 1));
+			if (word == NULL)
+				return (i + 1);
+			word = ft_fill_word(word, str, len);
+			array[i] = word;
+			str += len + 1;
 			i++;
 		}
 		else
 			str++;
 	}
-	array[i] = NULL;
+	array[ft_word_num(s, c)] = NULL;
+	return (0);
+}
+
+char	**ft_split_str(char const *str, char c)
+{
+	char	**array;
+	int		err;
+	int		i;
+
+	array = malloc(sizeof(char *) * (ft_word_num(str, c) + 1));
+	if (array == NULL)
+		return (NULL);
+	err = ft_split_low(array, str, c);
+	if (err)
+	{
+		i = 0;
+		while (i < err - 1)
+		{
+			free(array[i]);
+			i++;
+		}
+		free(array);
+		return (NULL);
+	}
 	return (array);
+}
+
+int	ft_correct_len(char *str, char *mark)
+{
+	unsigned int	i;
+	int				extra_mark;
+
+	i = 0;
+	extra_mark = 0;
+	while (str[i])
+	{
+		ft_mv_in_quote(str, &i);
+		if (ft_strchr(mark, str[i]))
+			extra_mark += 2;
+		i++;
+	}
+	// printf("%d %d\n", i, extra_mark);
+	return (i + extra_mark);
+}
+
+void	ft_fix_correct_str(char *ret, char *str, int *i, int *extra)
+{
+	char	quote;
+
+	if (str[*i] == '\"' || str[*i] == '\'')
+	{
+		quote = str[*i];
+		ret[*i + *extra] = str[*i];
+		(*i)++;
+		while (str[*i])
+		{
+			ret[*i + *extra] = str[*i];
+			if (str[*i] == quote)
+				break ;
+			(*i)++;
+		}
+		if (ft_strchr("<>|&", str[*i]))
+		{
+			ret[*i + *extra] = ' ';
+			ret[*i + *extra + 1] = str[*i];
+			ret[*i + *extra + 2] = ' ';
+			(*extra) += 2;
+		}
+		else
+			ret[*i + *extra] = str[*i];
+	}
+}
+
+char	*ft_correct_str(char *str)
+{
+	char	*ret;
+	int		i;
+	int		extra;
+
+	i = 0;
+	extra = 0;
+	ret = malloc(ft_correct_len(str, "<>|&") + 1);
+	if (!ret)
+		return (NULL);
+	while (str[i])
+	{
+		ft_fix_correct_str(ret, str, &i, &extra);
+		i++;
+	}
+	ret[i + extra] = '\0';
+	printf("%c\n", ret[0]);
+	return (ret);
 }
 
 int	main(int argc, char **argv, char **env)
 {
+	// char	*str = "$USER -la | grep \"Ma$USER ke file\"da $HOME";
+	// char	*str = "ls -la << EOF |awk \"<$HOME d\" | cat";
+	// char	*str = "ls -l | cat << lim | cat > outfile | cat >> outfile";
+	// char	*str = "ls -l | cat << lim | cat | cat";
+	// char	*str = "ls | cat | cat | cat > file | cat | cat |cat >> outfile";
 	char *str = "$USER -la | gre p \'Ma ke file\' $HOME";
 	char **res;
 	int i;
@@ -141,8 +307,11 @@ int	main(int argc, char **argv, char **env)
 	i = 0;
 	(void)argc;
 	(void)argv;
+	// str = ft_correct_str(str);
+	printf("%s\n", str);
 	res = ft_split_str(str, ' ');
 	input_daller(res, env);
+	// res = ft_correct_special(res, "<>|&");
 	while (res[i])
 	{
 		printf("%s\n", res[i]);
